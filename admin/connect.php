474da89a869cba36cce1b054d5e71ffad50d3c19
viethungtube <viethungtube@gmail.com>
2023-09-26 14:23:55 +0700

    Change user

--- admin/connect.php
+++ admin/connect.php
@@ -1,4 +1,4 @@
 <?php
-    $connect = mysqli_connect("localhost","root","","db_news");
+    $connect = mysqli_connect("localhost","group5","Secur4#Pa@sW0rld","db_news");
     mysqli_set_charset($connect,'utf8');
 ?>
--- admin/connect_nodb.php
+++ admin/connect_nodb.php
@@ -1,4 +1,4 @@
 <?php
-    $connect = mysqli_connect("localhost","root","");
+    $connect = mysqli_connect("localhost","group5","Secur4#Pa@sW0rld");
     mysqli_set_charset($connect,'utf8');
 ?>
